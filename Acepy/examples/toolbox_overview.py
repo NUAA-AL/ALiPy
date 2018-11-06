efe932012c3d5a0f78c48c840667a03b47de0b07
Ying-Peng Tang <tangyp@nuaa.edu.cn>
2018-11-06 23:03:10 +0800

    examples update

--- Acepy/examples/toolbox_overview.py
+++ Acepy/examples/toolbox_overview.py
@@ -1,7 +1,5 @@
 import copy
-
 from sklearn.datasets import load_iris
-
 from acepy.utils.toolbox import ToolBox
 
 X, y = load_iris(return_X_y=True)
@@ -13,30 +11,32 @@ acebox.split_AL(test_ratio=0.3, initial_label_rate=0.1, split_count=10)
 # Use the default Logistic Regression classifier
 model = acebox.get_default_model()
 
-# The cost budget is 50 times querying.
+# The cost budget is 50 times querying
 stopping_criterion = acebox.get_stopping_criterion('num_of_queries', 50)
 
-# Use pre-defined strategy, The data matrix is a reference which will not use additional memory
+# Use pre-defined strategy
 QBCStrategy = acebox.get_query_strategy(strategy_name='QBC')
-
 QBC_result = []
+
 for round in range(10):
     # Get the data split of one fold experiment
-    train_idx, test_idx, Lind, Uind = acebox.get_split(round)
-
+    train_idx, test_idx, label_ind, unlab_ind = acebox.get_split(round)
     # Get intermediate results saver for one fold experiment
     saver = acebox.get_stateio(round)
 
     while not stopping_criterion.is_stop():
         # Select a subset of Uind according to the query strategy
-        select_ind = QBCStrategy.select(Lind, Uind, model=model, batch_size=1)
-        Lind.update(select_ind)
-        Uind.difference_update(select_ind)
+        # Passing model=None to use the default model for evaluating the committees' disagreement
+        select_ind = QBCStrategy.select(label_ind, unlab_ind, model=None, batch_size=1)
+        label_ind.update(select_ind)
+        unlab_ind.difference_update(select_ind)
 
         # Update model and calc performance according to the model you are using
-        model.fit(X=X[Lind.index, :], y=y[Lind.index])
+        model.fit(X=X[label_ind.index, :], y=y[label_ind.index])
         pred = model.predict(X[test_idx, :])
-        accuracy = sum(pred == y[test_idx]) / len(test_idx)
+        accuracy = acebox.calc_performance_metric(y_true=y[label_ind.index],
+                                                  y_pred=pred,
+                                                  performance_metric='accuracy_score')
 
         # Save intermediate results to file
         st = acebox.State(select_index=select_ind, performance=accuracy)
@@ -52,4 +52,4 @@ for round in range(10):
 analyser = acebox.get_experiment_analyser(x_axis='num_of_queries')
 analyser.add_method('QBC', QBC_result)
 print(analyser)
-analyser.plot_learning_curves(title='make_classification')
+analyser.plot_learning_curves(title='Iris', std_area=True)
