517b14996ca333c8b1ba1f22f258f224a88133b5
Ying-Peng Tang <tangyp@nuaa.edu.cn>
2018-12-20 22:16:58 +0800

    AFASMC complete

--- Acepy/acepy/query_strategy/base.py
+++ Acepy/acepy/query_strategy/base.py
@@ -45,7 +45,7 @@ class BaseNoisyOracleQuery(BaseQueryStrategy, metaclass=ABCMeta):
 
     @abstractmethod
     def select(self, label_index, unlabel_index, batch_size=1, **kwargs):
-        """Query from oracles. Return the selected instance, cost and label.
+        """Query from oracles. Return the selected instance and oracle.
 
         Parameters
         ----------
@@ -61,8 +61,7 @@ class BaseNoisyOracleQuery(BaseQueryStrategy, metaclass=ABCMeta):
 
 
 class BaseMultiLabelQuery(BaseIndexQuery, metaclass=ABCMeta):
-    """
-    """
+    """Base query strategy for multi label setting."""
 
     def _check_multi_label_ind(self, container):
         """Check if the given array is an array of multi label indexes."""
@@ -73,9 +72,11 @@ class BaseMultiLabelQuery(BaseIndexQuery, metaclass=ABCMeta):
                 else:
                     container = MultiLabelIndexCollection.construct_by_1d_array(container, label_mat_shape=self.y.shape)
             except:
-                raise ValueError("Please pass a 1d array of indexes or MultiLabelIndexCollection or a list "
-                                 "of tuples with 2 elements, in which, the 1st element is the index of instance "
-                                 "and the 2nd element is the index of label.")
+                raise ValueError(
+                    "Please pass a 1d array of indexes or MultiLabelIndexCollection (column major, "
+                    "start from 0) or a list "
+                    "of tuples with 2 elements, in which, the 1st element is the index of instance "
+                    "and the 2nd element is the index of label.")
         return container
 
     def _check_multi_label(self, matrix):
@@ -101,4 +102,72 @@ class BaseMultiLabelQuery(BaseIndexQuery, metaclass=ABCMeta):
 
     @abstractmethod
     def select(self, label_index, unlabel_index, batch_size=1, **kwargs):
-        pass
+        """Select a subset from the unlabeled set, return the selected instance and label.
+
+        Parameters
+        ----------
+        label_index: {list, np.ndarray, MultiLabelIndexCollection}
+            The indexes of labeled samples. It should be a 1d array of indexes (column major, start from 0) or
+            MultiLabelIndexCollection or a list of tuples with 2 elements, in which,
+            the 1st element is the index of instance and the 2nd element is the index of labels.
+
+        unlabel_index: {list, np.ndarray, MultiLabelIndexCollection}
+            The indexes of unlabeled samples. It should be a 1d array of indexes (column major, start from 0) or
+            MultiLabelIndexCollection or a list of tuples with 2 elements, in which,
+            the 1st element is the index of instance and the 2nd element is the index of labels.
+
+        batch_size: int, optional (default=1)
+            Selection batch size.
+        """
+
+
+class BaseFeatureQuery(BaseMultiLabelQuery, metaclass=ABCMeta):
+    """Base query strategy for feature querying setting.
+    Basically have the same api with multi label setting."""
+
+    def _check_feature_ind(self, container):
+        if not isinstance(container, MultiLabelIndexCollection):
+            try:
+                if isinstance(container[0], tuple):
+                    container = MultiLabelIndexCollection(container, self.X.shape[1])
+                else:
+                    container = MultiLabelIndexCollection.construct_by_1d_array(container, label_mat_shape=self.X.shape)
+            except:
+                raise ValueError(
+                    "Please pass a 1d array of indexes or MultiLabelIndexCollection (column major, start from 0)"
+                    "or a list of tuples with 2 elements, in which, the 1st element is the index of instance "
+                    "and the 2nd element is the index of features.")
+        return container
+
+    def __init__(self, X=None, y=None, **kwargs):
+        if X is not None and y is not None:
+            if isinstance(X, np.ndarray) and isinstance(y, np.ndarray):
+                # will not use additional memory
+                check_X_y(X, y, accept_sparse='csc', multi_output=True)
+                self.X = X
+                self.y = y
+            else:
+                self.X, self.y = check_X_y(X, y, accept_sparse='csc', multi_output=True)
+        else:
+            self.X = X
+            self.y = y
+
+    @abstractmethod
+    def select(self, observed_entries, unkonwn_entries, batch_size=1, **kwargs):
+        """Select a subset from the unlabeled set, return the selected instance and feature.
+
+        Parameters
+        ----------
+        observed_entries: {list, np.ndarray, MultiLabelIndexCollection}
+            The indexes of labeled samples. It should be a 1d array of indexes (column major, start from 0)
+            or MultiLabelIndexCollection or a list of tuples with 2 elements, in which,
+            the 1st element is the index of instance and the 2nd element is the index of features.
+
+        unkonwn_entries: {list, np.ndarray, MultiLabelIndexCollection}
+            The indexes of unlabeled samples. It should be a 1d array of indexes (column major, start from 0)
+            or MultiLabelIndexCollection or a list of tuples with 2 elements, in which,
+            the 1st element is the index of instance and the 2nd element is the index of features.
+
+        batch_size: int, optional (default=1)
+            Selection batch size.
+        """
--- Acepy/acepy/query_strategy/query_features.py
+++ Acepy/acepy/query_strategy/query_features.py
@@ -16,10 +16,11 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
 import copy
-from scipy.sparse import csr_matrix
 
+import numpy as np
+
+from .base import BaseFeatureQuery
 from ..index.index_collections import MultiLabelIndexCollection
 
 
@@ -80,16 +81,11 @@ def AFASMC_mc(X, y, omega, **kwargs):
 
     Returns
     -------
-    X_mc: array
+    Xmc: array
         The completed matrix.
-
-    minObj: float
-        The value of objective function.
-
-    convergence: array
-        The array that records the values of objective function.
     """
-    max_iter = kwargs.pop('max_iter', 100)
+    max_in_iter = kwargs.pop('max_in_iter', 100)
+    max_out_iter = kwargs.pop('max_out_iter', 10)
     lambda1 = kwargs.pop('lambda1', 1)
     lambda2 = kwargs.pop('lambda2', 1)
     X = np.asarray(X)
@@ -97,77 +93,168 @@ def AFASMC_mc(X, y, omega, **kwargs):
     n_samples, n_features = X.shape
     lambda2 /= n_samples
 
-    X_obr = np.zeros(n_samples * n_features)
-    X_obr[omega] = X.flatten(order='F')[omega]
-    X_obr = X_obr.reshape((n_features, n_samples)).T
-    if isinstance(omega[0], tuple):
-        omega = MultiLabelIndexCollection(omega, X.shape[1])
-    else:
-        omega = MultiLabelIndexCollection.construct_by_1d_array(omega, label_mat_shape=X.shape)
-    obrT = np.asarray(omega.get_matrix_mask(label_mat_shape=X.shape, sparse_format='lil_matrix').todense())
+    # X_obr = np.zeros(n_samples * n_features)
+    # X_obr[omega] = X.flatten(order='F')[omega]
+    # X_obr = X_obr.reshape((n_features, n_samples)).T
+    if not isinstance(omega, MultiLabelIndexCollection):
+        try:
+            if isinstance(omega[0], tuple):
+                omega = MultiLabelIndexCollection(omega, X.shape[1])
+            else:
+                omega = MultiLabelIndexCollection.construct_by_1d_array(omega, label_mat_shape=X.shape)
+        except:
+            raise ValueError("Please pass a 1d array of indexes or MultiLabelIndexCollection "
+                             "(column major, start from 0) or a list "
+                             "of tuples with 2 elements, in which, the 1st element is the index of instance "
+                             "and the 2nd element is the index of features.")
+
+    obrT = np.asarray(omega.get_matrix_mask(mat_shape=X.shape, sparse_format='lil_matrix').todense())
     # X_obr = np.ma.array(data=X, mask=obrT.todense(), fill_value=0)
     # mask = (obrT == 1).todense()
     # X_obr[mask == 1] = X[mask == 1]
+    X_obr = np.zeros((n_samples, n_features))
+    X_obr += obrT * X
 
     theta0 = 1
     theta1 = 1
     Z0 = np.zeros((n_samples, n_features))
-    Z1 = Z0
+    Z1 = np.zeros((n_samples, n_features))
     ineqLtemp0 = np.zeros((n_samples, n_features))
     ineqLtemp1 = ineqLtemp0
     L = 2
-    convergence = np.zeros((max_iter, 1))
+    converge_out = np.zeros((max_in_iter, 1))
 
-    # train a linear model whose obj function is min{norm(Xw-Y)}
-    X_extend = np.hstack((X_obr, np.ones((n_samples, 1))))
-    W = np.linalg.pinv(X_extend.T.dot(X_extend)).dot(X_extend.T).dot(y)
-    w = W[0:-1].flatten()
-    b = W[-1]
+    for i in range(max_out_iter):
+        # train a linear model whose obj function is min{norm(Xw-Y)}
+        X_extend = np.hstack((X_obr, np.ones((n_samples, 1))))
+        W = np.linalg.pinv(X_extend.T.dot(X_extend)).dot(X_extend.T).dot(y)
+        w = W[0:-1].flatten()
+        b = W[-1]
 
-    for k in range(max_iter):
-        Y = Z1 + theta1 * (1 / theta0 - 1) * (Z1 - Z0)
-        svd_obj_temp_temp = (theta1 * (1 / theta0 - 1) + 1) * ineqLtemp1 - theta1 * (
-        1 / theta0 - 1) * ineqLtemp0 - X_obr
-        svd_obj_temp = svd_obj_temp_temp + 2 * lambda2 * (Y.dot(w) + b - y).reshape(-1, 1).dot(w.reshape((1, -1)))
-        svd_obj = Y - 1 / L * svd_obj_temp
-        Z0 = Z1
-        Z1, traceNorm = _svd_threshold(svd_obj, lambda1 / L)
+        convergence = np.zeros(max_in_iter)
 
-        ineqLtemp0 = ineqLtemp1
-        # do not know whether it is element wise or not
-        ineqLtemp1 = Z1 * obrT
-        ineqL = np.linalg.norm(ineqLtemp1 - X_obr, ord='fro') ** 2 / 2 + sum((Z1.dot(w) + b - y) ** 2) * lambda2
-
-        ineqRtemp = sum(sum(svd_obj_temp_temp ** 2)) / 2 + sum((Y.dot(w) + b - y) ** 2) * lambda2 - svd_obj_temp.flatten().dot(
-            Y.flatten())
-        ineqR = ineqRtemp + svd_obj_temp.flatten().dot(Z1.flatten()) + L / 2 * sum(sum((Z1 - Y) ** 2))
-
-        while ineqL > ineqR:
-            L = L * 2
+        for k in range(max_in_iter):
+            Y = Z1 + theta1 * (1 / theta0 - 1) * (Z1 - Z0)
+            svd_obj_temp_temp = (theta1 * (1 / theta0 - 1) + 1) * ineqLtemp1 - theta1 * (
+                1 / theta0 - 1) * ineqLtemp0 - X_obr
+            svd_obj_temp = svd_obj_temp_temp + 2 * lambda2 * (Y.dot(w) + b - y).reshape(-1, 1).dot(w.reshape((1, -1)))
             svd_obj = Y - 1 / L * svd_obj_temp
+            Z0 = copy.copy(Z1)
             Z1, traceNorm = _svd_threshold(svd_obj, lambda1 / L)
 
+            ineqLtemp0 = ineqLtemp1
+            # do not know whether it is element wise or not
             ineqLtemp1 = Z1 * obrT
             ineqL = np.linalg.norm(ineqLtemp1 - X_obr, ord='fro') ** 2 / 2 + sum((Z1.dot(w) + b - y) ** 2) * lambda2
+
+            ineqRtemp = sum(sum(svd_obj_temp_temp ** 2)) / 2 + sum(
+                (Y.dot(w) + b - y) ** 2) * lambda2 - svd_obj_temp.flatten().dot(
+                Y.flatten())
             ineqR = ineqRtemp + svd_obj_temp.flatten().dot(Z1.flatten()) + L / 2 * sum(sum((Z1 - Y) ** 2))
 
-        theta0 = theta1
-        theta1 = (np.sqrt(theta1 ** 4 + 4 * theta1 ** 2) - theta1 ** 2) / 2
+            while ineqL > ineqR:
+                L = L * 2
+                svd_obj = Y - 1 / L * svd_obj_temp
+                Z1, traceNorm = _svd_threshold(svd_obj, lambda1 / L)
 
-        convergence[k, 0] = ineqL + lambda1 * traceNorm
+                ineqLtemp1 = Z1 * obrT
+                ineqL = np.linalg.norm(ineqLtemp1 - X_obr, ord='fro') ** 2 / 2 + sum((Z1.dot(w) + b - y) ** 2) * lambda2
+                ineqR = ineqRtemp + svd_obj_temp.flatten().dot(Z1.flatten()) + L / 2 * sum(sum((Z1 - Y) ** 2))
 
-        # judge convergence
-        if k == 0:
-            minObj = convergence[k, 0]
-            X_mc = Z1
-        else:
-            if convergence[k, 0] < minObj:
-                minObj = convergence[k, 0]
+            theta0 = theta1
+            theta1 = (np.sqrt(theta1 ** 4 + 4 * theta1 ** 2) - theta1 ** 2) / 2
+
+            convergence[k] = ineqL + lambda1 * traceNorm
+
+            # judge convergence
+            if k == 0:
+                minObj = convergence[k]
                 X_mc = Z1
-        if k > 0:
-            if np.abs(convergence[k, 0] - convergence[k - 1, 0]) < ((1e-6) * convergence[k - 1, 0]):
+            else:
+                if convergence[k] < minObj:
+                    minObj = convergence[k]
+                    X_mc = Z1
+            if k > 0:
+                if np.abs(convergence[k] - convergence[k - 1]) < ((1e-6) * convergence[k - 1]):
+                    break
+
+        converge_out[i] = minObj
+        if i == 0:
+            minObj_out = converge_out[k]
+            Xmc = X_mc
+        else:
+            if converge_out[k] < minObj_out:
+                minObj_out = converge_out[k]
+                Xmc = X_mc
+        if i > 0:
+            if abs(converge_out[k] - converge_out[k - 1]) < (1e-4 * converge_out[k - 1]):
                 break
 
-    return X_mc, minObj, convergence
+    return (1-obrT)*Xmc + obrT*X
 
 
+class QueryFeatureAFASMC(BaseFeatureQuery):
+    """This class implement the KDD'18: Active Feature Acquisition with
+    Supervised Matrix Completion (AFASMC) method. It will complete the
+    matrix with supervised information first. And select the missing feature
+    with the highest variance based on the results of previous completion.
+
+    Parameters
+    ----------
+    X: 2D array, optional (default=None)
+        Feature matrix of the whole dataset. It is a reference which will not use additional memory.
+
+    y: array-like, optional (default=None)
+        Label matrix of the whole dataset. It is a reference which will not use additional memory.
+
+    train_idx: array-like
+        the index of training data.
+
+    References
+    ----------
+    [1] Huang, S.; Jin, R.; and Zhou, Z. 2014. Active learning by
+        querying informative and representative examples. IEEE
+        Transactions on Pattern Analysis and Machine Intelligence
+        36(10):1936–1949
+
+    """
+
+    def __init__(self, X, y, train_idx=None):
+        super(QueryFeatureAFASMC, self).__init__(X, y)
+        self._train_idx = train_idx
+        self._feature_variance = []
+
+    def select(self, observed_entries, unkonwn_entries, **kwargs):
+        # build map from value to index
+        if len(unkonwn_entries) <= 1:
+            return unkonwn_entries
+        unkonwn_entries = self._check_feature_ind(unkonwn_entries)
+        observed_entries = self._check_feature_ind(observed_entries)
+
+        if self._train_idx is None:
+            obi = observed_entries.get_instance_index()
+            uni = unkonwn_entries.get_instance_index()
+            self._train_idx = np.union1d(obi, uni)
+
+        # map entries of the whole data to the training data
+        tr_ob = []
+        for entry in observed_entries:
+            if entry[0] in self._train_idx:
+                ind_in_train = np.where(self._train_idx == entry[0])[0][0]
+                tr_ob.append((ind_in_train, entry[1]))
+            else:
+                tr_ob.append(entry)
+        tr_ob = MultiLabelIndexCollection(tr_ob)
+
+        # matrix completion
+        X_mc = AFASMC_mc(X=self.X[self._train_idx], y=self.y[self._train_idx], omega=tr_ob)
+        self._feature_variance.append(X_mc)
+        mc_sh = np.shape(X_mc)
+        var_mat = np.zeros(mc_sh)
+        for i in range(mc_sh[0]):
+            for j in range(mc_sh[1]):
+                var_mat[i, j] = np.var([mat[i][j] for mat in self._feature_variance])
+        var_mat *= 1-np.asarray(tr_ob.get_matrix_mask(mat_shape=mc_sh, sparse_format='lil_matrix').todense())
+        selected_feature = np.argmax(var_mat)   # a 1d index in training set
+
+        return [(self._train_idx[selected_feature//self.X.shape[1]], selected_feature % self.X.shape[1])]
