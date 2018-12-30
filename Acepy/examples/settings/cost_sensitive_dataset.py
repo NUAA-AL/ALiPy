c514f5d9d30483b85f4a96d1a0ede590ef4c335c
BlackroseLi <3123652@qq.com>
2018-12-30 10:51:44 +0800

    rm test
    Former-commit-id: 350e5538dff2528abbc348c46b5157a53a5d2b03

--- Acepy/examples/settings/cost_sensitive_dataset.py
+++ Acepy/examples/settings/cost_sensitive_dataset.py
@@ -1,20 +1,24 @@
-
-
-labels = [0, 1 ,0]
-cost = [2, 1, 2]
+import sys
+sys.path.append(r'C:\Users\31236\Desktop\al_tools\acepy')
+import numpy as np 
+labels = [0, 1 , 0, 3]
+print(np.unique(labels))
+cost = [2, 1, 2, 5]
 from acepy.oracle import Oracle
 oracle = Oracle(labels=labels, cost=cost)
 
-labels, cost = oracle.query_by_index(indexes=[1])
+labels, cost = oracle.query_by_index(indexes=np.unique(labels))
 
-from acepy.experiment import State
-st = State(select_index=select_ind, performance=accuracy, cost=cost)
+print(labels)
+print(cost)
+# from acepy.experiment import State
+# st = State(select_index=select_ind, performance=accuracy, cost=cost)
 
-radom_result = [[(1, 0.6), (2, 0.7), (2, 0.8), (1, 0.9)],
-                [(1, 0.7), (1, 0.7), (1.5, 0.75), (2.5, 0.85)]]  # 2 folds, 4 queries for each fold.
-uncertainty_result = [saver1, saver2]  # each State object in the saver must have the 'cost' entry.
-from acepy.experiment import ExperimentAnalyser
+# radom_result = [[(1, 0.6), (2, 0.7), (2, 0.8), (1, 0.9)],
+#                 [(1, 0.7), (1, 0.7), (1.5, 0.75), (2.5, 0.85)]]  # 2 folds, 4 queries for each fold.
+# uncertainty_result = [saver1, saver2]  # each State object in the saver must have the 'cost' entry.
+# from acepy.experiment import ExperimentAnalyser
 
-analyser = ExperimentAnalyser(x_axis='cost')
-analyser.add_method('random', radom_result)
-analyser.add_method('uncertainty', uncertainty_result)
+# analyser = ExperimentAnalyser(x_axis='cost')
+# analyser.add_method('random', radom_result)
+# analyser.add_method('uncertainty', uncertainty_result)
