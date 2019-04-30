818bc594b600a0bfd15e0f4998d07e7bed773e5a
Ying-Peng Tang <tangyp@nuaa.edu.cn>
2019-04-30 17:03:56 +0800

    add AURO method to the __init__ file

--- alipy/query_strategy/__init__.py
+++ alipy/query_strategy/__init__.py
@@ -3,5 +3,5 @@ from .cost_sensitive import QueryCostSensitiveHALC, QueryCostSensitiveRandom, Qu
 from .multi_label import QueryMultiLabelQUIRE, QueryMultiLabelAUDI, QueryMultiLabelMMC, QueryMultiLabelAdaptive, QueryMultiLabelRandom
 from .noisy_oracles import QueryNoisyOraclesCEAL, QueryNoisyOraclesIEthresh, QueryNoisyOraclesAll, QueryNoisyOraclesRandom
 from .query_features import QueryFeatureAFASMC, QueryFeatureRandom, QueryFeatureStability
-from .query_type import check_query_type
+from .query_type import check_query_type, QueryTypeAURO
 from .cost_sensitive import select_Knapsack_01, select_POSS, hierarchical_multilabel_mark
